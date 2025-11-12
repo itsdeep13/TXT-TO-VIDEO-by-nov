f4eba539ebe0929f05ed1604aad1e10333e17253
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-11-12 17:37:20 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,22 +2,23 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "20937420"))
-API_HASH = environ.get("API_HASH", "09d7f6744feb17759304df65666961da")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "22484497"))
+API_HASH = environ.get("API_HASH", "c38cb053916c47a97590c244663cbaef")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8220091800:AAHuIONRiJ6B0NMDItwp5_z8Z84CX-n6gv0")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "selectionway_free_course")  # Channel username without @, 
 FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/selectionway_free_course")  # Channel link
 
 # Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "8183010692").split()))
+ADMINS = list(map(int, environ.get("ADMINS", "6252997817").split()))
 
 # Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", "8183010692"))
+OWNER_ID = int(environ.get("OWNER_ID", "6252997817"))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "")
+
 
 
 
